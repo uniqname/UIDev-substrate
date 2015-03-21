b99c439c994b6a25a4c4211db8fd2741d67efe4d
Andrew Kendall <kendall130@yahoo.com>
2015-03-21 09:50:15 -0600

    setting up the framework for the slider decided to use a Angular base for this project

--- js/app.js
+++ js/app.js
@@ -1,2 +1,85 @@
-angular.module('website', ['ngAnimate'])
-  .controller('MainCtrl', function($scope) { });
+Skip to content
+ This repository
+Explore
+Gist
+Blog
+Help
+@akendall akendall
+
+ Watch 4
+  Star 53
+ Fork 44simpulton/angular-photo-slider
+ branch: master  angular-photo-slider/js/app.js
+Lukas Ruebbelkesimpulton on Mar 12, 2014 Updated to v 1.2.14 and changed addClass to beforeAddClass
+1 contributor
+RawBlameHistory     69 lines (58 sloc)  2.487 kb
+angular.module('website', ['ngAnimate', 'ngTouch'])
+    .controller('MainCtrl', function ($scope) {
+        $scope.slides = [
+            {image: 'images/img00.jpg', description: 'Image 00'},
+            {image: 'images/img01.jpg', description: 'Image 01'},
+            {image: 'images/img02.jpg', description: 'Image 02'},
+            {image: 'images/img03.jpg', description: 'Image 03'},
+            {image: 'images/img04.jpg', description: 'Image 04'}
+        ];
+
+        $scope.direction = 'left';
+        $scope.currentIndex = 0;
+
+        $scope.setCurrentSlideIndex = function (index) {
+            $scope.direction = (index > $scope.currentIndex) ? 'left' : 'right';
+            $scope.currentIndex = index;
+        };
+
+        $scope.isCurrentSlideIndex = function (index) {
+            return $scope.currentIndex === index;
+        };
+
+        $scope.prevSlide = function () {
+            $scope.direction = 'left';
+            $scope.currentIndex = ($scope.currentIndex < $scope.slides.length - 1) ? ++$scope.currentIndex : 0;
+        };
+
+        $scope.nextSlide = function () {
+            $scope.direction = 'right';
+            $scope.currentIndex = ($scope.currentIndex > 0) ? --$scope.currentIndex : $scope.slides.length - 1;
+        };
+    })
+    .animation('.slide-animation', function () {
+        return {
+            beforeAddClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    var finishPoint = element.parent().width();
+                    if(scope.direction !== 'right') {
+                        finishPoint = -finishPoint;
+                    }
+                    TweenMax.to(element, 0.5, {left: finishPoint, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            },
+            removeClass: function (element, className, done) {
+                var scope = element.scope();
+
+                if (className == 'ng-hide') {
+                    element.removeClass('ng-hide');
+
+                    var startPoint = element.parent().width();
+                    if(scope.direction === 'right') {
+                        startPoint = -startPoint;
+                    }
+
+                    TweenMax.fromTo(element, 0.5, { left: startPoint }, {left: 0, onComplete: done });
+                }
+                else {
+                    done();
+                }
+            }
+        };
+    });
+
+Status API Training Shop Blog About
+© 2015 GitHub, Inc. Terms Privacy Security Contact
